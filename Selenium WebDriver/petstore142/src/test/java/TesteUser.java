7aac10ef12b9c6c9f82de3ec6bc7b3c950ef112e
Danillo.sdd <danillo.sdd@hotmail.com>
2025-05-20 17:23:05 -0300

    Update

--- Selenium WebDriver/petstore142/src/test/java/TesteUser.java
+++ Selenium WebDriver/petstore142/src/test/java/TesteUser.java
@@ -2,7 +2,6 @@
 
 // 1 - bibliotecas
 // 2 - classe
-
 import org.junit.jupiter.api.Test;
 
 public class TesteUser {
@@ -12,9 +11,22 @@ public class TesteUser {
     static String uriUser = "https://petstore.swagger.io/v2/user"; // Base URL + endpoint
 
     // 2.2 - funções e métodos
+    // 2.2.1 - funções e métodos comuns / uteis
+    // Função de leitura do arquivo JSON
+    public static String lerArquivoJson(String arquivoJson) {
+
+        // ToDo: "Completar a leitura do arquivo JSON"
+        //return new String(Files)
+        return null;
+
+    }
+
+    // 2.2.2 - métodos de teste
     @Test
-    public void testPostPet(){
-        // carregar os dados do arquivo JSON do pet         
-        String jsonBody = 
+    public void testPostPet() {
+        // carregar os dados do arquivo JSON do pet         ￼￼￼
+        
+        // ToDo: "Criar o método Post
+        //String jsonBody = 
     }
 }
