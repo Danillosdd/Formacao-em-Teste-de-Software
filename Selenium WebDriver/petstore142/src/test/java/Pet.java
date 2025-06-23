6ad2be0a71eb08c41154b6032dc805a3bb0b18fc
danillo.sdd <danillo.sdd@hotmail.com>
2025-06-23 20:19:32 -0300

    Update

--- Selenium WebDriver/petstore142/src/test/java/Pet.java
+++ Selenium WebDriver/petstore142/src/test/java/Pet.java
@@ -2,10 +2,23 @@ public class Pet {
 
     // Definindo uma classe chamada Pet para guardar a estrutura de dados sobre
     // animais(Pet)
-    public int petId; // Parâmetro do ID do pet
-    public String petName; // Parâmetro do nome do pet
-    public int catId; // Parâmetro do ID da categoria
-    public String catName; // Parâmetro do nome da categoria
+    public int Id; // Parâmetro do ID do pet
+
+    public class Category { // Classe interna para representar a categoria do pet
+        public int id; // Parâmetro do ID da categoria
+        public String name; // Parâmetro do nome da categoria
+    } // Fim da classe interna Category
+
+    public String Name; // Parâmetro do nome do pet
+    public String[] photoUrls; // Parâmetro das URLs das fotos do pet
+
+    public class Tag { // Classe interna para representar as tags do pet
+        public int id; // Parâmetro do ID da tag
+        public String name; // Parâmetro do nome da tag
+    } // Fim da classe interna Tag
+
+    public Tag[] tags; // Parâmetro das tags do pet
+
     public String status; // Parâmetro do status do pet
 
 }
