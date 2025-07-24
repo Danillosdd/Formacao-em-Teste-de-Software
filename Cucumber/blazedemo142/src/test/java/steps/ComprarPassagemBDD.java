4ce267319af2656676c68aaf3b132ecdb1859364
danillo.sdd <danillo.sdd@hotmail.com>
2025-07-24 17:38:26 -0300

    Update

--- Cucumber/blazedemo142/src/test/java/steps/ComprarPassagemBDD.java
+++ Cucumber/blazedemo142/src/test/java/steps/ComprarPassagemBDD.java
@@ -1,11 +1,33 @@
 package steps;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.E;
 import io.cucumber.java.pt.Entao;
 import io.cucumber.java.pt.Quando;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class ComprarPassagemBDD {
+
+    WebDriver driver; // objeto do Selenium WebDriver
+
+    @Before
+    public void iniciar() {
+        WebDriverManager.chromedriver().setup(); // configura o WebDriver para o Chrome
+        driver = new ChromeDriver(); // instancia como ChromeDriver
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(3000)); // espera implícita de 3 segundos
+    }
+
+    @After
+    public void finalizar() {
+
+    }
 
     @Dado("que acesso o site: {string}")
     public void que_acesso_o_site(String String) {
