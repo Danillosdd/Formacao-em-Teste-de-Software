de703c962d04a4c45b3fd4ea4a3ead5e065f5f5f
danillo.sdd <danillo.sdd@hotmail.com>
2025-08-04 18:42:56 -0300

    Update

--- Cucumber/blazedemoadv142/src/test/java/steps/ComprarPassagemBDD.java
+++ Cucumber/blazedemoadv142/src/test/java/steps/ComprarPassagemBDD.java
@@ -2,7 +2,6 @@ package steps;
 
 import java.time.Duration;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
