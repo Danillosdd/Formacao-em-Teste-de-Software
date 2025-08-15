483f08101d16a7c9225f63b23b1f738de12435f8
danillo.sdd <danillo.sdd@hotmail.com>
2025-08-15 00:16:21 -0300

    update

--- Cucumber/blazedemoadv142/src/test/java/pages/ReservePage.java
+++ Cucumber/blazedemoadv142/src/test/java/pages/ReservePage.java
@@ -1,5 +1,23 @@
 package pages;
 
-public class ReservePage {
-    
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ReservePage extends CommonPage {
+
+    // construtor
+    public ReservePage(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(css = "h3")
+    private WebElement flightsHeader;
+
+    // ações
+    public String lerCabeçalhoVoos() {
+        return flightsHeader.getText();
+    }
 }
