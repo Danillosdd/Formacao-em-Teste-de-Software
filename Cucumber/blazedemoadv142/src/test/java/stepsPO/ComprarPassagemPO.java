0ae7c5a8027e5d1b4b82348cae516b7eeb61f215
danillo.sdd <danillo.sdd@hotmail.com>
2025-08-19 01:07:30 -0300

    update

--- Cucumber/blazedemoadv142/src/test/java/stepsPO/ComprarPassagemPO.java
+++ Cucumber/blazedemoadv142/src/test/java/stepsPO/ComprarPassagemPO.java
@@ -14,6 +14,7 @@ public class ComprarPassagemPO {
 
     //Atributos - Ponto Final / Estação Final / Terminal
     final WebDriver driver; // Selenium só pode usar e devolver
+    // 1 -Declarar
     private HomePage homePage; // Só eu posso usar a Home
     private ReservePage reservePage; // Só eu posso usar a Reserve
 
@@ -24,8 +25,8 @@ public class ComprarPassagemPO {
     // As anotações Before e After ficam no Hooks.java
     @Dado("que acesso o site {string} PO")
     public void que_acesso_o_site_po(String url) {
-        homePage = new HomePage(driver);
-        homePage.acessarHomePage(url);
+        homePage = new HomePage(driver); // 2 - Instanciar
+        homePage.acessarHomePage(url); // 3 - Usar 
 
     }
 
