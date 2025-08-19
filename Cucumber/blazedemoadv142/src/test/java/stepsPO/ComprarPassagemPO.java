ff746a05d8562af9ac9d889de3e08f7a108834f1
danillo.sdd <danillo.sdd@hotmail.com>
2025-08-19 00:36:57 -0300

    update

--- Cucumber/blazedemoadv142/src/test/java/stepsPO/ComprarPassagemPO.java
+++ Cucumber/blazedemoadv142/src/test/java/stepsPO/ComprarPassagemPO.java
@@ -1,27 +1,29 @@
 package stepsPO;
 
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.E;
 import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
 
 public class ComprarPassagemPO {
 
     @Dado("que acesso o site {string} PO")
-public void que_acesso_o_site_po(String string) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}
-@Quando("seleciono a {string} e {string} PO")
-public void seleciono_a_e_po(String string, String string2) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}
-@Quando("clico no botao Find Flights PO")
-public void clico_no_botao_find_flights_po() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}
-@Entao("visualiza a lista de voos PO")
-public void visualiza_a_lista_de_voos_po() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}
+    public void que_acesso_o_site_po(String string) {
+       
+    }
+
+    @Quando("seleciono a {string} e {string} PO")
+    public void seleciono_a_e_po(String string, String string2) {
+
+    }
+
+    @E("clico no botao Find Flights PO")
+    public void clico_no_botao_find_flights_po() {
+
+    }
+
+    @Entao("visualiza a lista de voos PO")
+    public void visualiza_a_lista_de_voos_po() {
+
+    }
 }
