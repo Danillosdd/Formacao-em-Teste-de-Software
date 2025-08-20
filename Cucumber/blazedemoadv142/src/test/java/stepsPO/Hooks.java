6d9bdd04a3a1e80f87b07b237366c2bd45affb16
danillo.sdd <danillo.sdd@hotmail.com>
2025-08-19 23:52:03 -0300

    update

---
+++ Cucumber/blazedemoadv142/src/test/java/stepsPO/Hooks.java
@@ -0,0 +1,20 @@
+package stepsPO;
+
+import io.cucumber.java.Before;
+import pages.Base;
+
+public class Hooks {
+
+    Base base;
+
+    // Construtor
+    public Hooks(Base base) {
+        this.base = base;
+    }
+
+    @Before
+    public void iniciarPO() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+}
