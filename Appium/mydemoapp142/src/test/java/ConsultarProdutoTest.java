b959f6e52d593e6bc790cb17335b621baba697f4
danillo.sdd <danillo.sdd@hotmail.com>
2025-09-09 22:47:00 -0300

    update

--- Appium/mydemoapp142/src/test/java/ConsultarProdutoTest.java
+++ Appium/mydemoapp142/src/test/java/ConsultarProdutoTest.java
@@ -49,9 +49,7 @@ public class ConsultarProdutoTest {
 
     @Test
     public void sampleTest() {
-        var el1 = driver.findElement(AppiumBy.xpath("(//android.widget.ImageView[@content-desc=\"Product Image\"])[1]\n"
-                + //
-                ""));
+        var el1 = driver.findElement(AppiumBy.xpath("(//android.widget.ImageView[@content-desc=\"Product Image\"])[1]"));
         el1.click();
         var el2 = driver.findElement(AppiumBy.id("com.saucelabs.mydemoapp.android:id/productTV"));
         el2.click();
