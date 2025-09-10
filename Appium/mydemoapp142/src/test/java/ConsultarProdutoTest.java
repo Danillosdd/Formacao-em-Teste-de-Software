30e0c05e2854f00a43f5038e3f7f46f91c44cfc6
danillo.sdd <danillo.sdd@hotmail.com>
2025-09-09 22:25:40 -0300

    update

--- Appium/mydemoapp142/src/test/java/ConsultarProdutoTest.java
+++ Appium/mydemoapp142/src/test/java/ConsultarProdutoTest.java
@@ -16,7 +16,7 @@ public class ConsultarProdutoTest {
 
     private AndroidDriver driver;
 
-    private URL pegarUrl() {
+    private URL getUrl() {
         try {
             return new URL("https://ondemand.eu-central-1.saucelabs.com/wd/hub");
         } catch (MalformedURLException e) {
@@ -45,7 +45,7 @@ public class ConsultarProdutoTest {
                 .amend("webSocketUrl", true)
                 .amend("unhandledPromptBehavior", "ignore");
 
-        driver = new AndroidDriver(this.pegarUrl(), options);
+        driver = new AndroidDriver(this.getUrl(), options);
     }
 
     @Test
