793ff92b2b0b338b044b324440f3e3b66a10cf7a
danillo.sdd <danillo.sdd@hotmail.com>
2025-09-11 00:13:45 -0300

    update

--- Appium/mydemoapp142/src/test/java/steps/ComprarProduto.java
+++ Appium/mydemoapp142/src/test/java/steps/ComprarProduto.java
@@ -2,13 +2,14 @@ package steps;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.interactions.PointerInput;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.openqa.selenium.interactions.Sequence;
 
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
@@ -19,7 +20,6 @@ import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.E;
 import io.cucumber.java.pt.Entao;
 import io.cucumber.java.pt.Quando;
-import java.time.Duration;
 
 public class ComprarProduto {
 
@@ -103,16 +103,16 @@ public class ComprarProduto {
         driver.findElement(AppiumBy.id("com.soucelabs.mydemoapp.android:id/addToCartButton")).click();
 
         // Arrastar para cima
-        final var finger = new PointerInput(PointerInput.Kind.TOUCH, name:"finger");
-        var start = new Point(x:525, y:1698);
-        var end = new Point(x:530, y:563);
+        final var finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
+        var start = new Point(525, 1698);
+        var end = new Point(530, 563);
         var swipe = new Sequence(finger, 1);
-    
-        swipe.addAction(finger.createPointerMove(Duration.ofMillis(0), 
-            PointerInput.Origin.viewport(), start.getX(), start.getY()));
+
+        swipe.addAction(finger.createPointerMove(Duration.ofMillis(0),
+                PointerInput.Origin.viewport(), start.getX(), start.getY()));
         swipe.addAction(finger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()));
-        swipe.addAction(finger.createPointerMove(Duration.ofMillis(1000), 
-            PointerInput.Origin.viewport(), end.getX(), end.getY()));
+        swipe.addAction(finger.createPointerMove(Duration.ofMillis(1000),
+                PointerInput.Origin.viewport(), end.getX(), end.getY()));
         swipe.addAction(finger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
         driver.perform(Arrays.asList(swipe));
     }
