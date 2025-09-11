23c1607c52dc9f55aeae00570593d5b1e35dc0e6
danillo.sdd <danillo.sdd@hotmail.com>
2025-09-10 23:33:14 -0300

    update

--- Appium/mydemoapp142/src/test/java/steps/ComprarProduto.java
+++ Appium/mydemoapp142/src/test/java/steps/ComprarProduto.java
@@ -75,8 +75,8 @@ public class ComprarProduto {
         // Home
         // produto :
         // preco   :
-        assertEquals("Sauce Labs Backpack", driver.findElement(AppiumBy.xpath("//android.widget.TextView[@content-desc=\"Product title\" and @text=\"Sauce Labs Backpack\"]")).getText());
-        assertEquals("$ 29.99", driver.findElement(AppiumBy.xpath("//android.widget.TextView[@content-desc=\"Product price\" and @text=\"$ 29.99\"]")).getText());
+        assertEquals("Sauce Labs Backpack", driver.findElement(AppiumBy.xpath("//android.widget.TextView[@content-desc=\"Product title\" and @text=" + produto + "]")).getText());
+        assertEquals("$ 29.99", driver.findElement(AppiumBy.xpath("//android.widget.TextView[@content-desc=\"Product price\" and @text=" + preco + "]")).getText());
     }
 
     @Quando("clico na imagem do {int}")
