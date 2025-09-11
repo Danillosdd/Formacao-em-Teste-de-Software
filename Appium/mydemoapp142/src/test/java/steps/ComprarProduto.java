b49b772373cb588eaa47b4cf9cb2073d68c63a50
danillo.sdd <danillo.sdd@hotmail.com>
2025-09-10 22:10:28 -0300

    update

--- Appium/mydemoapp142/src/test/java/steps/ComprarProduto.java
+++ Appium/mydemoapp142/src/test/java/steps/ComprarProduto.java
@@ -1,11 +1,52 @@
 package steps;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.options.BaseOptions;
+import io.cucumber.java.Before;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.E;
 import io.cucumber.java.pt.Entao;
 import io.cucumber.java.pt.Quando;
 
 public class ComprarProduto {
+
+    private AndroidDriver driver;
+
+    private URL getUrl() {
+        try {
+            return new URL("https://danillo.sdd:d16f7bb2-9729-4aa5-ae9a-4bc95891689f@ondemand.us-west-1.saucelabs.com:443/wd/hub");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Before
+    public void iniciar() {
+        var options = new BaseOptions()
+                .amend("platformName", "Android")
+                .amend("appium:platformVersion", "9.0")
+                .amend("appium:deviceName", "Samsung Galaxy S9 FHD GoogleAPI Emulator")
+                .amend("appium:deviceOrientation", "portrait")
+                .amend("appium:app", "storage:filename=mda-2.2.0-25.apk")
+                .amend("appium:appPackage", "com.saucelabs.mydemoapp.android")
+                .amend("appium:appActivity", "com.saucelabs.mydemoapp.android.view.activities.SplashActivity")
+                .amend("appium:automationName", "UiAutomator2")
+                .amend("browserName", "")
+                .amend("appium:ensureWebviewsHavePages", true)
+                .amend("appium:nativeWebScreenshot", true)
+                .amend("sauce:options", Map.ofEntries(Map.entry("name", "Appium Desktop Session -- Sep 9, 2025 9:55 PM")))
+                .amend("appium:newCommandTimeout", 3600)
+                .amend("appium:connectHardwareKeyboard", true)
+                .amend("webSocketUrl", true)
+                .amend("unhandledPromptBehavior", "ignore");
+
+        driver = new AndroidDriver(this.getUrl(), options);
+    }
 
     @Dado("que acesso o My Demo App")
     public void que_acesso_o_my_demo_app() {
