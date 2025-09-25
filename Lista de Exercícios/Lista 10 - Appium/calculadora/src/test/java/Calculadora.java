b4b04ec1f9deb1a30e58205813df10c60e2df7de
danillo.sdd <danillo.sdd@hotmail.com>
2025-09-24 23:03:03 -0300

    update

--- Lista de Exercícios/Lista 10 - Appium/calculadora/src/test/java/Calculadora.java
+++ Lista de Exercícios/Lista 10 - Appium/calculadora/src/test/java/Calculadora.java
@@ -20,7 +20,7 @@ public class Calculadora {
     private URL getUrl() {
         try {
             return new URL(
-                    "https://oauth-danillo.sdd-1bce0:d8f4796c-46db-49a1-937f-d3547dab1442@ondemand.us-west-1.saucelabs.com:443/wd/hub");
+                    "https://oauth-danillo-75a98:49d13ae3-a890-47ed-813b-6ab9262151c7@ondemand.us-west-1.saucelabs.com:443/wd/hub");
         } catch (MalformedURLException e) {
             e.printStackTrace();
             return null;
