25a7af3b5caaef6821fe6e699150b9fcc6556f15
danillo.sdd <danillo.sdd@hotmail.com>
2025-09-24 23:36:58 -0300

    update

--- Lista de Exercícios/Lista 10 - Appium/calculadora/src/test/java/Calculadora.java
+++ Lista de Exercícios/Lista 10 - Appium/calculadora/src/test/java/Calculadora.java
@@ -73,10 +73,9 @@ public class Calculadora {
         driver.findElement(AppiumBy.id("com.google.android.calculator:id/digit_2")).click();
         driver.findElement(AppiumBy.id("com.google.android.calculator:id/op_add")).click();
         driver.findElement(AppiumBy.id("com.google.android.calculator:id/digit_3")).click();
-        driver.findElement(AppiumBy.id("com.google.android.calculator:id/eq")).click();
 
         // Verifica o resultado
-        var resultado = driver.findElement(AppiumBy.id("com.google.android.calculator:id/result"));
+        var resultado = driver.findElement(AppiumBy.id("com.google.android.calculator:id/result_preview"));
         assertEquals("5", resultado.getText());
     }
 
